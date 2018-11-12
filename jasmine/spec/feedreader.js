458a6c7cac3d6dfbea7132b81f59bc69892e054d
Scott Jensen <scottj91.sj@gmail.com>
2018-11-12 00:33:24 -0700

    Update jasmine/spec/feedreader.js
    Loop through name defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -43,6 +43,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+        it('name is defined and not empty', function() {
+            for (var i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].name).toBeDefined();
+                expect(allFeeds[i].name.length > 0).toBe(true);
+            }
+        });
     });
 
 
